335cc3ab6cc1cf19c28ba8672e9176a5c99f77e0
SumithraRajendran <saianeka315@gmail.com>
2023-08-06 12:15:40 +0530

    created project

---
+++ travello/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+
+class TravelloConfig(AppConfig):
+    name = 'travello'
+
+class BlogConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'my_app'
